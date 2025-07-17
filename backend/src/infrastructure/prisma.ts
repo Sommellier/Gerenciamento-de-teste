dae01d91376e5ab952e7d832dc514d06582becaa
Sommellier <richardriedo87@gmail.com>
2025-07-17 16:25:56 -0300

    teste

--- backend/src/infrastructure/prisma.ts
+++ backend/src/infrastructure/prisma.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 
 if (!process.env.DATABASE_URL) {
+  require('dotenv').config({ path: '.env' })
+}
+
+if (!process.env.DATABASE_URL) {
   throw new Error('❌ DATABASE_URL não está definida. Verifique seu .env ou jest.setup.js');
 }
 
