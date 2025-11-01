46e6a379aed3cf4e99659ad7651dc9e52c2e8bce
Sommellier <richardriedo87@gmail.com>
2025-11-01 16:02:08 -0300

    teste workflow

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,8 @@
 import 'dotenv/config'
 import app from './server'
 
-const PORT = process.env.PORT ? Number(process.env.PORT) : 3000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+// Azure App Service usa variável PORT automaticamente
+const PORT = Number(process.env.PORT) || 8080
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`)
+})
