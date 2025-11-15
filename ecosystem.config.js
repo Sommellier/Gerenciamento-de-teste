04b63f1c75ff961fc69519b8dd69153c6d9d1f3f
Sommellier <richardriedo87@gmail.com>
2025-11-15 16:44:12 -0300

    Novo teste surpremo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -21,15 +21,15 @@ module.exports = {
       "pre-deploy-local": "echo 'Starting deployment...'",
       "pre-deploy": "sudo mkdir -p /var/www/api-backend/{source,shared,current,releases} /var/www/shared && sudo chown -R ubuntu:ubuntu /var/www/api-backend /var/www/shared && (test -d /var/www/api-backend/source/.git || (cd /var/www/api-backend/source && git clone git@github.com:Sommellier/Gerenciamento-de-teste.git . || git clone https://github.com/Sommellier/Gerenciamento-de-teste.git .))",
       "post-deploy": [
-        "cp -n /var/www/shared/.env backend/.env || true",
-        "cd backend",
+        "cp -n /var/www/shared/.env /var/www/api-backend/current/backend/.env || true",
+        "cd /var/www/api-backend/current/backend",
         "npm ci",
         "npm run build",
         "npx prisma generate",
         "npx prisma migrate deploy || true",
         "npm prune --omit=dev",
-        "cd ..",
-        "pm2 startOrReload ecosystem.config.js --env production"
+        "cd /var/www/api-backend/current",
+        "pm2 startOrReload /var/www/api-backend/current/ecosystem.config.js --env production"
       ].join(" && ")
     }
   }
